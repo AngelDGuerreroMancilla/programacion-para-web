914b153aca3bde938b50b99efc35dbf2aa19a2b6
angel guerrero <agurero36@ucol.mx>
2026-04-19 18:33:50 -0600

    cambio de puerto

--- semana7/api.js
+++ semana7/api.js
@@ -1,8 +1,16 @@
 import express from 'express'
+import path from "path"
+import { fileURLToPath } from "url"
 
 const app = express();
 app.use(express.json());
-app.use(express.static('.'));
+
+
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname  = path.dirname(__filename)
+app.use(express.static(path.join(__dirname, "public")))
+
 
 
 const arrayCarros = [];
@@ -61,6 +69,8 @@ app.delete('/vehiculos/:id',(req,res)=>{
 
 })
 
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000')
-})
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log("Server is running on port" + PORT);
+});
